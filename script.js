40003c8b34d47da4aeb9bf6dde9256f58cba363d
Danielomes <danielrosilvad@gmail.com>
2024-05-08 23:19:12 -0300

    gerente

--- script.js
+++ script.js
@@ -13,3 +13,23 @@
 
 // Chama a função de efeito de glitch após o carregamento da página
 window.onload = glitchEffect;
+
+// ********************
+// SENHA DO CODIGO
+// ********************
+function showLoginForm() {
+    var form = document.getElementById("loginForm");
+    form.style.display = "block"; // Mostra o formulário quando a imagem é clicada
+}
+
+function checkPassword() {
+    var password = document.getElementById("password").value;
+    // Aqui você verifica se a senha está correta
+    if (password === "oi") {
+        window.location.href = "fitas.html"; // Redireciona para outra página se a senha estiver correta
+        return false; // Impede o envio do formulário
+    } else {
+     
+        return false; // Impede o envio do formulário
+    }
+}
