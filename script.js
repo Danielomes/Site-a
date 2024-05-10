0924f751e5cafc227a11b5c8273d26a200a8ad21
Danielomes <danielrosilvad@gmail.com>
2024-05-09 22:37:21 -0300

    Finished

--- script.js
+++ script.js
@@ -24,12 +24,12 @@ function showLoginForm() {
 
 function checkPassword() {
     var password = document.getElementById("password").value;
+    
     // Aqui você verifica se a senha está correta
     if (password === "PMStra") {
         window.location.href = "fitas.html"; // Redireciona para outra página se a senha estiver correta
         return false; // Impede o envio do formulário
     } else {
-     
         return false; // Impede o envio do formulário
     }
 }
